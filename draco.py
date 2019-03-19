60ee851800c9da13638a35b9bb7cd52dd064aeb1
gt22 <igorengel@mail.ru>
2019-03-19 20:31:54 +0300

    Parallel json processing, and 0.81

--- draco.py
+++ draco.py
@@ -1,8 +1,8 @@
 # %%
+from websocket import create_connection
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
-import xgboost
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from catboost import CatBoostClassifier
@@ -10,6 +10,26 @@ import os
 import json
 import tqdm
 from itertools import chain
+from multiprocessing import Pool
+from collections import defaultdict
+import gc
+# %%
+
+
+def init_websocket():
+    ws = create_connection("ws://10.8.0.1:8080/alert")
+    return ws
+
+
+def alert(msg: str='Computation complete', ws: bool=True, usr: str='Admin') -> str:
+    if not ws:
+        return "Alerted"
+    else:
+        uadab = init_websocket()
+        uadab.send(f"alert${usr}:{msg.replace(' ', ':')}")
+        rep = uadab.recv()
+        uadab.shutdown()
+        return rep
 
 
 # %%
@@ -21,12 +41,18 @@ def get_data(name):
 
 # %%
 df = pd.read_csv(get_data("skill_train.csv")).set_index('id')
+test_df = pd.read_csv(get_data('skill_test.csv')).set_index('id')
 # %%
 heroes = pd.read_csv(get_data('heroes.csv')).set_index('hero_id')
 abils = pd.read_csv(get_data('abilities.csv')).set_index('ability_id')
 abils_behavior = set(chain.from_iterable(abils['behavior']
                                          .apply(lambda x: eval(x.replace('nan', 'None'))).values))
 items = pd.read_csv(get_data('items.csv')).set_index('item_id')
+
+with open(get_data('skill_train.jsonlines')) as f:
+    json_line = json.loads(f.readline())
+    for c in df.columns:
+        json_line.pop(c, None)
 # %%
 
 
@@ -35,11 +61,19 @@ def map_df(d, columns, m):
         d[c] = d[c].map(m)
 
 
+def read_single_json():
+    with open(get_data('skill_train.jsonlines')) as f:
+        line = f.readline()
+        rec = json.loads(line)
+        for c in df.columns:
+            rec.pop(c, None)
+        return rec
 # %%
 
 
 def preprocess(d):
-    team_map = {'radiant': 0, 'dire': 1, 'other': 2}
+    team_map = {'radiant': 0, 'dire': 1}
+    d.drop(d[d['winner_team'] == 'other'].index, inplace=True)
     map_df(d, ['player_team', 'winner_team'], team_map)
     d['is_winner'] = (d['player_team'] == d['winner_team']).astype(int)
     for b in abils_behavior:
@@ -48,36 +82,82 @@ def preprocess(d):
         d[f'upgrade_beh_list_{b}'] = 0
     for item in items.index:
         d[f'item_{item}'] = 0
+    for hero in heroes.index:
+        d[f'p_hero_{hero}'] = 0
+        d[f'e_hero_{hero}'] = 0
     d['item_0'] = 0
+    d['ult_time'] = 0
+
+
+def z():
+    return 0
+
+
+def process_json_line(l):
+    d = defaultdict(z)
+    rec = json.loads(l)
+    d['id'] = rec['id']
+    lvl = rec['level_up_times']
+    for b in rec['ability_upgrades']:
+        d[f'upgrade_beh_list_{abils.loc[b, "behavior"]}'] += 1
+        for bh in eval(abils.loc[b, 'behavior'].replace("nan", "None")):
+            d[f'upgrade_beh_{bh}'] += 1
+    # for item in rec['final_items']:
+    #     d.loc[i, f'item_{item}'] += 1
+    for item in rec['item_purchase_log']:
+        d[f'item_{item["item_id"]}'] += 1
+    p_team, e_team = ('radiant', 'dire') if rec['player_team'] == 'radiant' else ('dire', 'radiant')
+    for p_hero in rec[f'{p_team}_heroes']:
+        d[f'p_hero_{p_hero}'] += 1
+    for e_hero in rec[f'{e_team}_heroes']:
+        d[f'e_hero_{e_hero}'] += 1
+    d['ult_time'] = -1 if len(lvl) < 9 else lvl[8]
+    return d
 
 
 def preprocess_json(d, f):
-    d['ult_time'] = 0
-    with open(f) as j:
-        for line in tqdm.tqdm(j, total=len(d)):
-            rec = json.loads(line)
-            i = rec['id']
-            lvl = rec['level_up_times']
-            for b in rec['ability_upgrades']:
-                d.loc[i, f'upgrade_beh_list_{abils.loc[b, "behavior"]}'] += 1
-                for bh in eval(abils.loc[b, 'behavior'].replace("nan", "None")):
-                    d.loc[i, f'upgrade_beh_{bh}'] += 1
-            for item in rec['final_items']:
-                d.loc[i, f'item_{item}'] += 1
-
-            d.loc[i, 'ult_time'] = -1 if len(lvl) < 9 else lvl[8]
+        print("Starting...")
+        p = Pool()
+        res = p.map(process_json_line, f)
+        p.close()
+        del p
+        print("Wrapping to df")
+        dd = pd.DataFrame(res).set_index('id')
+        print("Copying to df")
+        d = d.update(dd)
+        print("Complete")
+        return d
 
 
 def preprocess_after_json(d):
     d['ult_percent'] = d['ult_time'] / d['duration']
 
 
+def read_json(f):
+    with open(f) as fi:
+        ret = fi.readlines()
+    return ret
+
+
 # %%
 preprocess(df)
-# %%
-preprocess_json(df, get_data('skill_train.jsonlines'))
-# %%
+jd = read_json(get_data('skill_train.jsonlines'))
+preprocess_json(df, jd)
+del jd
+gc.collect()
 preprocess_after_json(df)
+alert("Train json complete")
+# %%
+preprocess(test_df)
+jd = read_json(get_data('skill_test.jsonlines'))
+preprocess_json(test_df, jd)
+del jd
+gc.collect()
+preprocess_after_json(test_df)
+alert("Test json complete")
+# %%
+df = pd.read_csv(get_data("processed_train.csv"), index_col='id')
+test_df = pd.read_csv(get_data("processed_test.csv"), index_col='id')
 # %%
 target = 'skilled'
 
@@ -161,11 +241,6 @@ model = CatBoostClassifier(
 model.fit(X_train, y_train, cat_features=cat_id, eval_set=(X_test, y_test))
 
 score_model(model.predict, 'catboost')
-# %%
-test_df = pd.read_csv(get_data('skill_test.csv')).set_index('id')
-preprocess(test_df)
-preprocess_json(test_df, get_data('skill_test.jsonlines'))
-preprocess_after_json(test_df)
 # %%
 
 
