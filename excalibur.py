8466d332e50edf4f802af4053089e02e7e3fdcd6
gt22 <igorengel@mail.ru>
2019-03-20 13:33:36 +0300

    Rework excalibur, add mean from series, 0.824 score

--- excalibur.py
+++ excalibur.py
@@ -1,54 +1,134 @@
-# %%
-from websocket import create_connection
 import pandas as pd
 import numpy as np
-import os
 import json
 from itertools import chain
 from multiprocessing import Pool
 from collections import defaultdict
 import gc
-# %%
-
-
-def init_websocket():
-    ws = create_connection("ws://10.8.0.1:8080/alert")
-    return ws
-
-
-def alert(msg: str='Computation complete', ws: bool=True, usr: str='Admin') -> str:
-    if not ws:
-        return "Alerted"
-    else:
-        uadab = init_websocket()
-        uadab.send(f"alert${usr}:{msg.replace(' ', ':')}")
-        rep = uadab.recv()
-        uadab.shutdown()
-        return rep
-
-
-# %%
+from typing import List
 
 
 def get_data(name):
     return f'data/dota2_{name}'
 
 
-# %%
-df = pd.read_csv(get_data("skill_train.csv")).set_index('id')
-test_df = pd.read_csv(get_data('skill_test.csv')).set_index('id')
-# %%
 heroes = pd.read_csv(get_data('heroes.csv')).set_index('hero_id')
 abils = pd.read_csv(get_data('abilities.csv')).set_index('ability_id')
 abils_behavior = set(chain.from_iterable(abils['behavior']
                                          .apply(lambda x: eval(x.replace('nan', 'None'))).values))
 items = pd.read_csv(get_data('items.csv')).set_index('item_id')
 
-with open(get_data('skill_train.jsonlines')) as f:
-    json_line = json.loads(f.readline())
-    for c in df.columns:
-        json_line.pop(c, None)
-# %%
+
+class ProcessingModule:
+
+    def process(self, l: dict, d: dict):
+        pass
+
+    def get_cols(self) -> List[str]:
+        return []
+
+
+class Pipeline(ProcessingModule):
+    modules: List[ProcessingModule]
+
+    def __init__(self, *args):
+        self.modules = args
+
+    def process(self, l: dict, d: dict):
+        for m in self.modules:
+            m.process(l, d)
+
+    def get_cols(self):
+        return sum((m.get_cols() for m in self.modules), [])
+
+
+class UltTime(ProcessingModule):
+
+    def process(self, l: dict, d: dict):
+        lvl = l['level_up_times']
+        d['ult_time'] = -1 if len(lvl) < 9 else lvl[8]
+        d['ult_percent'] = d['ult_time'] / l['duration'] if d['ult_time'] > 0 else -1
+
+    def get_cols(self):
+        return ['ult_time', 'ult_percent']
+
+
+class AbilityUpgrades(ProcessingModule):
+
+    def process(self, l: dict, d: dict):
+        for b in l['ability_upgrades']:
+            d[f'upgrade_beh_list_{abils.loc[b, "behavior"]}'] += 1
+            for bh in eval(abils.loc[b, 'behavior'].replace("nan", "None")):
+                d[f'upgrade_beh_{bh}'] += 1
+
+    def get_cols(self):
+        return [f'upgrade_beh_list_{b}' for b in abils['behavior']] + \
+               [f'upgrade_beh_{b}' for b in abils_behavior]
+
+
+class Items(ProcessingModule):
+
+    def process(self, l: dict, d: dict):
+        for item in l['item_purchase_log']:
+            d[f'item_{item["item_id"]}'] += 1
+
+    def get_cols(self):
+        return [f'item_{i}' for i in items.index]
+
+
+class Heroes(ProcessingModule):
+
+    def process(self, l: dict, d: dict):
+        p_team, e_team = ('radiant', 'dire') if l['player_team'] == 'radiant' else ('dire', 'radiant')
+        for p_hero in l[f'{p_team}_heroes']:
+            d[f'p_hero_{p_hero}'] += 1
+        for e_hero in l[f'{e_team}_heroes']:
+            d[f'e_hero_{e_hero}'] += 1
+
+    def get_cols(self):
+        return [f'p_hero_{i}' for i in items.index] + [f'e_hero_{i}' for i in items.index]
+
+
+class Series(ProcessingModule):
+
+    def process(self, l: dict, d: dict):
+        p_team, e_team = ('radiant', 'dire') if l['player_team'] == 'radiant' else ('dire', 'radiant')
+        ser = l['series']
+        d['player_gold_mean'] = np.mean(ser['player_gold'])
+        d['player_team_gold_mean'] = np.mean(ser[f'{p_team}_gold'])
+        d['enemy_team_gold_mean'] = np.mean(ser[f'{e_team}_gold'])
+
+    def get_cols(self):
+        return ['player_gold_mean', 'enemy_team_gold_mean', 'player_team_gold_mean']
+
+
+def z():
+    return 0
+
+
+def process_json_line(m: ProcessingModule, l: str):
+    d = defaultdict(z)
+    rec = json.loads(l)
+    d['id'] = rec['id']
+    m.process(rec, d)
+    return d
+
+
+def preprocess_json(d: pd.DataFrame, data: List[str], m: ProcessingModule):
+    print("Starting...")
+    p = Pool()
+    res = p.starmap(process_json_line, [(m, x) for x in data])
+    p.close()
+    del p
+    print("Adding cols to df")
+    for c in m.get_cols():
+        d[c] = 0
+    print("Wrapping to df")
+    dd = pd.DataFrame(res).set_index('id')
+    print("Copying to df")
+    d = d.update(dd)
+    print("Complete")
+    return d
 
 
 def map_df(d, columns, m):
@@ -56,80 +136,11 @@ def map_df(d, columns, m):
         d[c] = d[c].map(m)
 
 
-def read_single_json():
-    with open(get_data('skill_train.jsonlines')) as f:
-        line = f.readline()
-        rec = json.loads(line)
-        for c in df.columns:
-            rec.pop(c, None)
-        return rec
-# %%
-
-
 def preprocess(d):
     team_map = {'radiant': 0, 'dire': 1}
     d.drop(d[d['winner_team'] == 'other'].index, inplace=True)
     map_df(d, ['player_team', 'winner_team'], team_map)
     d['is_winner'] = (d['player_team'] == d['winner_team']).astype(int)
-    for b in abils_behavior:
-        d[f'upgrade_beh_{b}'] = 0
-    for b in abils['behavior']:
-        d[f'upgrade_beh_list_{b}'] = 0
-    for item in items.index:
-        d[f'item_{item}'] = 0
-    for hero in heroes.index:
-        d[f'p_hero_{hero}'] = 0
-        d[f'e_hero_{hero}'] = 0
-    d['item_0'] = 0
-    d['ult_time'] = 0
-
-
-def z():
-    return 0
-
-
-def process_json_line(l):
-    d = defaultdict(z)
-    rec = json.loads(l)
-    d['id'] = rec['id']
-    lvl = rec['level_up_times']
-    for b in rec['ability_upgrades']:
-        d[f'upgrade_beh_list_{abils.loc[b, "behavior"]}'] += 1
-        for bh in eval(abils.loc[b, 'behavior'].replace("nan", "None")):
-            d[f'upgrade_beh_{bh}'] += 1
-    # for item in rec['final_items']:
-    #     d.loc[i, f'item_{item}'] += 1
-    for item in rec['item_purchase_log']:
-        d[f'item_{item["item_id"]}'] += 1
-    p_team, e_team = ('radiant', 'dire') if rec['player_team'] == 'radiant' else ('dire', 'radiant')
-    for p_hero in rec[f'{p_team}_heroes']:
-        d[f'p_hero_{p_hero}'] += 1
-    for e_hero in rec[f'{e_team}_heroes']:
-        d[f'e_hero_{e_hero}'] += 1
-    d['ult_time'] = -1 if len(lvl) < 9 else lvl[8]
-    ser = rec['series']
-    d['player_gold_mean'] = np.mean(ser['player_gold'])
-    d['player_team_gold_mean'] = np.mean(ser[f'{p_team}_gold'])
-    d['enemy_team_gold_mean'] = np.mean(ser[f'{e_team}_gold'])
-    return d
-
-
-def preprocess_json(d, f):
-        print("Starting...")
-        p = Pool()
-        res = p.map(process_json_line, f)
-        p.close()
-        del p
-        print("Wrapping to df")
-        dd = pd.DataFrame(res).set_index('id')
-        print("Copying to df")
-        d = d.update(dd)
-        print("Complete")
-        return d
-
-
-def preprocess_after_json(d):
-    d['ult_percent'] = d['ult_time'] / d['duration']
 
 
 def read_json(f):
@@ -138,22 +149,36 @@ def read_json(f):
     return ret
 
 
-# %%
-preprocess(df)
-jd = read_json(get_data('skill_train.jsonlines'))
-preprocess_json(df, jd)
-del jd
-gc.collect()
-preprocess_after_json(df)
-alert("Train json complete")
+print("Reading data")
+df = pd.read_csv(get_data("skill_train.csv")).set_index('id')
+test_df = pd.read_csv(get_data('skill_test.csv')).set_index('id')
 
+module = Pipeline(
+    UltTime(),
+    AbilityUpgrades(),
+    Items(),
+    Heroes(),
+    Series()
+)
+
+print("Preprocessing data")
+preprocess(df)
 preprocess(test_df)
-jd = read_json(get_data('skill_test.jsonlines'))
-preprocess_json(test_df, jd)
+
+print("Reading train json")
+jd = read_json(get_data('skill_train.jsonlines'))
+print("Processing train json")
+preprocess_json(df, jd, module)
 del jd
 gc.collect()
-preprocess_after_json(test_df)
-alert("Test json complete")
-# %%
+
+print("Reading test json")
+jd = read_json(get_data('skill_test.jsonlines'))
+print("Processing test json")
+preprocess_json(test_df, jd, module)
+del jd
+gc.collect()
+
+print("Saving")
 df.to_csv(get_data('processed_train.csv'))
 test_df.to_csv(get_data('processed_test.csv'))
